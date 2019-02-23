99a02845db0ba020b903a048e73d0081291b3dab
Shabanov Anton <snakesolid@ngs.ru>
2019-02-23 22:04:50 +0300

    added missed module

---
+++ src/handler/abort.rs
@@ -0,0 +1,38 @@
+use super::util::handle_request;
+use super::HandlerError;
+use crate::config::ConfigRef;
+use crate::jobmanager::JobManagerRef;
+use iron::middleware::Handler;
+use iron::IronResult;
+use iron::Request as IronRequest;
+use iron::Response as IronResponse;
+
+#[derive(Debug)]
+pub struct AbortHandler {
+    config: ConfigRef,
+    job_manager: JobManagerRef,
+}
+
+impl AbortHandler {
+    pub fn new(config: ConfigRef, job_manager: JobManagerRef) -> AbortHandler {
+        AbortHandler {
+            config,
+            job_manager,
+        }
+    }
+}
+
+impl Handler for AbortHandler {
+    fn handle(&self, request: &mut IronRequest) -> IronResult<IronResponse> {
+        handle_request(request, move |request: Request| {
+            self.job_manager
+                .set_aborted(request.jobid)
+                .map_err(|_| HandlerError::new("Failed to abort job"))
+        })
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Request {
+    jobid: usize,
+}
