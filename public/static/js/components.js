27496d08480c5f452d69b0426d436347666a6531
Shabanov Anton <snakesolid@ngs.ru>
2020-05-03 10:45:50 +0300

    move dialogs to end, adjusted export dialog height

--- public/static/js/components.js
+++ public/static/js/components.js
@@ -11,11 +11,6 @@ define(["knockout"], function(ko) {
 		template: { require: "text!components/Restore.html" },
 	});
 
-	ko.components.register("ko-parse-dialog", {
-		viewModel: { require: "components/ParseDialog" },
-		template: { require: "text!components/ParseDialog.html" },
-	});
-
 	ko.components.register("ko-status", {
 		viewModel: { require: "components/Status" },
 		template: { require: "text!components/Status.html" },
@@ -29,6 +24,13 @@ define(["knockout"], function(ko) {
 	ko.components.register("ko-settings", {
 		viewModel: { require: "components/Settings" },
 		template: { require: "text!components/Settings.html" },
+	});
+
+	// Dialogs
+
+	ko.components.register("ko-parse-dialog", {
+		viewModel: { require: "components/ParseDialog" },
+		template: { require: "text!components/ParseDialog.html" },
 	});
 
 	ko.components.register("ko-export-dialog", {
